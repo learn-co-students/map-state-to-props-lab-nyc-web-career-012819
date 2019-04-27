16c96c20f675279e1f53f23a93917e9fff524784
github username <github email address>
2019-04-27 18:44:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
 
-// add imports and code
+const store = createStore(manageUsers)
 
 
 ReactDOM.render(
-  // add imports and code
+  <Provider store={store}>
   <App />
-  // add imports and code
+  </Provider>
   ,
   document.getElementById('root')
 );
